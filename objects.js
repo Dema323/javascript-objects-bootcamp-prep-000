98579e7c95a7449609c8ca0f3e481c209eabb46a
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-22 12:38:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,20 +1,11 @@
-var playlist = {
-  'Foo Fighters': 'Everlong',
-  Prince: 'Purple Rain'
-}
+var playlist = {artistName : songTitle}
 
-function updatePlaylist(playlist, artist, song) {
-  // Remember, because one of this function's arguments
-  // is `playlist`, the `playlist` _inside_ the function
-  // is not the same as the top-level `playlist` outside
-  // the function
-  playlist[artist] = song
-
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle
   return playlist
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist]
-
-  return playlist
+function removeFromPlaylist(playlist, artistName){
+delete playlist.artistName
+return playlist
 }
